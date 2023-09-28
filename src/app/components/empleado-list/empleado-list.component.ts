c1415d9de078d70e27758a9770c68cf9d40f21da
jescolargonzalez <jesusescolargonzalez@gmail.com>
2023-09-28 12:38:33 +0200

    ComunicacionEntreComponentes

---
+++ src/app/components/empleado-list/empleado-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Empleado } from 'src/app/models/Empleado';
+
+@Component({
+  selector: 'app-empleado-list',
+  templateUrl: './empleado-list.component.html',
+  styleUrls: ['./empleado-list.component.css'],
+})
+export class EmpleadoListComponent implements OnInit {
+  /* Datos en bruto. */
+  listEmpleados: Empleado[] = [
+    { num: 1 , nombre: 'Devra' , apellido: 'Juajijajoija' , sexo: 'Femenino', salario: 77777 } ,
+    { num: 2 , nombre: 'Trasgo' , apellido: 'Perrinxi' , sexo: 'Masculino' , salario: 18000 } ,
+    { num: 3 , nombre: 'Palermo', apellido: 'Gatuno' , sexo: 'Masculino', salario: 10000 } ,
+    { num: 4 , nombre: 'Roco', apellido: 'Miedosin' , sexo: 'Masculino' , salario: 0 }
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+}
---
+++ src/app/models/Empleado.ts
@@ -0,0 +1,19 @@
+export class Empleado {
+
+  num: number;
+  nombre: String;
+  apellido: String;
+  sexo: String;
+  salario: number;
+
+  constructor(num:number ,nombre:string ,apellido:string ,sexo:string ,salario: number) {
+    this.num = num;
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.sexo = sexo;
+    this.salario = salario;
+  }
+
+
+
+}
