d278262588e6773a214fac6c3271b84ca26049d6
jescolargonzalez <jesusescolargonzalez@gmail.com>
2023-09-28 13:52:30 +0200

    :)

--- src/app/components/empleado-list/empleado-list.component.ts
+++ src/app/components/empleado-list/empleado-list.component.ts
@@ -9,15 +9,55 @@ import { Empleado } from 'src/app/models/Empleado';
 export class EmpleadoListComponent implements OnInit {
   /* Datos en bruto. */
   listEmpleados: Empleado[] = [
-    { num: 1 , nombre: 'Devra' , apellido: 'Juajijajoija' , sexo: 'Femenino', salario: 77777 } ,
-    { num: 2 , nombre: 'Trasgo' , apellido: 'Perrinxi' , sexo: 'Masculino' , salario: 18000 } ,
-    { num: 3 , nombre: 'Palermo', apellido: 'Gatuno' , sexo: 'Masculino', salario: 10000 } ,
-    { num: 4 , nombre: 'Roco', apellido: 'Miedosin' , sexo: 'Masculino' , salario: 0 }
+    {
+      num: 1,
+      nombre: 'Devra',
+      apellido: 'Juajijajoija',
+      sexo: 'Femenino',
+      salario: 77777,
+    },
+    {
+      num: 2,
+      nombre: 'Trasgo',
+      apellido: 'Perrinxi',
+      sexo: 'Masculino',
+      salario: 18000,
+    },
+    {
+      num: 3,
+      nombre: 'Palermo',
+      apellido: 'Gatuno',
+      sexo: 'Masculino',
+      salario: 10000,
+    },
+    {
+      num: 4,
+      nombre: 'Roco',
+      apellido: 'Miedosin',
+      sexo: 'Masculino',
+      salario: 0,
+    },
   ];
 
+  radioButtonSeleccionado = 'Todos';
   constructor() {}
 
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
+
+  obtenerTotalEmpleados(): number {
+    return this.listEmpleados.length;
+  }
+  obtenerTotalFemeninos(): number {
+    return this.listEmpleados.filter((list) => list.sexo === 'Femenino').length;
+  }
+  obtenerTotalMasculinos(): number {
+    return this.listEmpleados.filter((list) => list.sexo === 'Masculino')
+      .length;
+  }
+  empleadoCountRadioButtonChange(radioButtonSelec: string): void {
+    this.radioButtonSeleccionado = radioButtonSelec;
+  }
+
 }
